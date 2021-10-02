4c8b85d878c500f3617fd47d887897e086505ab9
Jarosław Przybył <jarekprzybyl1@gmail.com>
2021-10-02 22:05:05 +0200

    Remove frontend

--- libros-webapp/src/services/auth-header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    let token = JSON.parse(localStorage.getItem('token'));
-  
-    if (token) {
-      return { Authorization: 'Bearer ' + token }; 
-    } else {
-      return {};
-    }
-  }
