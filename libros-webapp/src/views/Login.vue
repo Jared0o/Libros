f5f3680d69fd1323bb46156607a6ed9ae5cc7acf
Jarosław Przybył <jarekprzybyl1@gmail.com>
2021-10-02 22:04:17 +0200

    s

--- libros-webapp/src/views/Login.vue
+++ libros-webapp/src/views/Login.vue
@@ -1,9 +1,29 @@
 <template>
-  <form class="form" @submit.prevent="handleLogin">
-    <input class="input" v-model="email" type="email" placeholder="email" />
-    <input class="input" v-model="password" type="password" placeholder="password" />
-    <button class="btn">Login</button>
+  <div class="w-full max-w-xs">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Email
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Hasło
+      </label>
+      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+      <p class="text-red-500 text-xs italic">Please choose a password.</p>
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Zaloguj
+      </button>
+      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+        Przypomnij hasło
+      </a>
+    </div>
   </form>
+</div>
 </template>
 
 <script>
